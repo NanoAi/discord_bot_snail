6c01f8b348964107be617862871108b423059799
NanoAi <luatenshi@gmail.com>
2024-07-19 13:31:25 -0400

    Added boolean converter.

---
+++ src/modules/convert.ts
@@ -0,0 +1,7 @@
+export function AsBoolean(value: string) {
+  if (!value)
+    return false
+  if (value === '1' || value === 'y' || value.toLowerCase() === 'true')
+    return true
+  return false
+}
