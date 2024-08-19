809cea251768cca92238bba761d34542bda7e029
NanoAi <luatenshi@gmail.com>
2024-08-19 12:56:28 -0400

    Updated Prisma Controller

--- src/modules/controllers/guildController.ts
+++ src/modules/controllers/guildController.ts
@@ -1,40 +1,97 @@
-import type { Guild, Ticket } from '@prisma/client'
+import type { Case, Guild, Punishment, Ticket } from '@prisma/client'
 import prisma from '../prisma'
 
-export interface Punishment {
-  issuer: string
-  reason: string
-  expiration: Date
-}
-
 class GuildController {
-  private static empty = { create: [] }
-  private guild?: string
+  private guildId?: string
 
-  constructor() {}
-
-  where(guild: string) {
-    this.guild = guild
+  where(guildId: string) {
+    this.guildId = guildId
+    return this
   }
 
-  async upsertGuild(xpSystem: boolean = false) {
-    const empty = GuildController.empty
+  // Create or update a Guild
+  async upsertGuild(xpSystem: boolean = false): Promise<Guild> {
     return await prisma.guild.upsert({
-      where: { guildId: this.guild },
+      where: { guildId: this.guildId! },
       update: { xpSystem },
-      create: { guildId: this.guild!, xpSystem: false, bans: empty, mutes: empty, tickets: empty },
+      create: {
+        guildId: this.guildId!,
+        xpSystem,
+      },
     })
   }
 
-  async getGuild() {
+  // Find a Guild by guildId
+  async findGuild(): Promise<Guild | null> {
     return await prisma.guild.findUnique({
-      where: { guildId: this.guild! },
+      where: { guildId: this.guildId! },
     })
   }
 
-  async addTicket(creatorId: string, ticketTitle: string, ticketTag: string) {
+  // Create a new Case
+  async createCase(caseId: number, description: string): Promise<Case> {
+    return await prisma.case.create({
+      data: {
+        guildId: this.guildId!,
+        caseId,
+        description,
+      },
+    })
+  }
+
+  // Find a Case by caseId
+  async findCase(caseId: number): Promise<Case | null> {
+    return await prisma.case.findFirst({
+      where: { guildId: this.guildId, caseId },
+    })
+  }
+
+  // Create a new Punishment (warn, ban, mute)
+  async createPunishment(
+    guildId: string,
+    issuer: string,
+    reason: string,
+    expiration: Date,
+    type: 'warns' | 'bans' | 'mutes',
+  ): Promise<Punishment> {
+    return await prisma.punishment.create({
+      data: {
+        guildId,
+        issuer,
+        reason,
+        expiration,
+      },
+      include: {
+        [type]: true,
+      },
+    })
+  }
+
+  // Create a new Ticket
+  async createTicket(ticketId: number, creatorId: string, ticketTitle: string, ticketTag: string): Promise<Ticket> {
     return await prisma.ticket.create({
-      data: { guildId: this.guild!, creatorId, ticketTitle, ticketTag },
+      data: {
+        guildId: this.guildId!,
+        ticketId,
+        creatorId,
+        ticketTitle,
+        ticketTag,
+      },
+    })
+  }
+
+  // Find all Tickets for a guild
+  async findTickets(guildId: string): Promise<Ticket[]> {
+    return await prisma.ticket.findMany({
+      where: { guildId },
+    })
+  }
+
+  // Update a Ticket's status
+  async updateTicketStatus(ticketId: number, newStatus: string) {
+    return await prisma.ticket.updateMany({
+      where: { guildId: this.guildId!, ticketId },
+      data: { ticketTag: newStatus },
     })
   }
 }
