07d4f03b4ad41743a06518baf5434e8dbe188477
NanoAi <luatenshi@gmail.com>
2024-09-09 23:40:16 -0400

    removed useless code

--- src/modules/utils/logger.ts
+++ src/modules/utils/logger.ts
@@ -39,13 +39,6 @@ export const logger: pino.Logger & { catchError: typeof catchError, catchFatal: 
 export function bindLogger() {
   const client = Discord.Client
   client.once(Events.ClientReady, readyClient => logger.info(`Logged in as ${readyClient.user.tag}`))
-
-  // I honestly don't think this ever happens...
-  client.on(Events.GuildUpdate, (oldGuild, newGuild) => {
-    if (oldGuild.id !== newGuild.id)
-      logger.info(`[${oldGuild.id}] => [${newGuild.id}]`)
-  })
-
   client.on(Events.Debug, info => logger.debug(info))
   client.on(Events.Warn, info => logger.warn(info))
   client.on(Events.Error, error => logger.error(error))
